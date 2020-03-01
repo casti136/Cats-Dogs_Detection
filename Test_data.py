83f691d8ca1e1fe9ecaa8ae34823f3a2a54d61a7
yvielcastillejos <60524557+yvielcastillejos@users.noreply.github.com>
2020-02-29 21:42:15 -0500

    Create Test_data.py

---
+++ Test_data.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+import csv
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import Conv2D, Dropout, MaxPooling2D,Activation
+
+X = []
+Y = []
+
+# Retrieve Labels
+#with open("DataY.csv") as csvfile:
+ #   reader = csv.reader(csvfile)
+  #  for row in reader: # each row is a list
+   #     Yy.append(row)
+#for element in Yy:
+ #   Y.append(int(element[0]))
+
+# Retrieve X Values
+pickle_in = open("X.pickle","rb")
+X = pickle.load(pickle_in)
+pickle_in = open("Y.pickle","rb")
+Y = pickle.load(pickle_in)
+
+
+#normalize X
+Y = np.array(Y)
+X = tf.keras.utils.normalize(X, axis=1)
+#print(X[1])
+#1
+model = tf.keras.models.Sequential()
+model.add(Conv2D(64,(3,3),input_shape = X.shape[1:])) #batchsize, 50 x 50 , 1 is rgb
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size =(2,2)))
+#2
+model.add(Conv2D(64,(3,3))) #batchsize, 50 x 50 , 1 is rgb
+model.add(Activation("relu"))
+model.add(MaxPooling2D(pool_size =(2,2)))
+model.add(tf.keras.layers.Flatten())
+#3
+model.add(tf.keras.layers.Dense(64))
+#Output layer
+model.add(tf.keras.layers.Dense(1, activation =tf.nn.sigmoid))
+
+model.compile(optimizer='adam',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+model.fit(X, Y, batch_size=32, epochs =2, validation_split=0.1)
+
